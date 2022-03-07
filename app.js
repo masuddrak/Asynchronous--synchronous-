fe22b947b36fba01797c7dd9b5d6299a2226cc93
masudrana <masudranaacb@gmail.com>
2022-03-07 23:00:45 +0600

    fetch using

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 console.log('give me 1 taka')
 console.log('give me 2 taka')
+
 setTimeout(() => {
     console.log('give me 10 taka')
 }, 5000);
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+
 setTimeout(() => {
-    console.log('give me 10 taka')
+    console.log('give me 20 taka')
 }, 4000);
+
 console.log('give me 3 taka')
 console.log('give me 4 taka')
