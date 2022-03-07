03919ee597f574f856c60205c061c345c0316ee3
masudrana <masudranaacb@gmail.com>
2022-03-07 22:46:24 +0600

    using set-time out

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log('give me 1 taka')
+console.log('give me 2 taka')
+setTimeout(() => {
+    console.log('give me 10 taka')
+}, 5000);
+setTimeout(() => {
+    console.log('give me 10 taka')
+}, 4000);
+console.log('give me 3 taka')
+console.log('give me 4 taka')
